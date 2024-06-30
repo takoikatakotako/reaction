552461c76d25ffc15ad69231415a898767c8721f
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2024-06-30 10:30:30 +0900

    反応機構リストのリファクタリング

--- Reaction/View/ReactionList/ReactionListRow.swift
+++ Reaction/View/ReactionList/ReactionListRow.swift
@@ -4,27 +4,21 @@ struct ReactionListRow: View {
     let reactionMechanism: ReactionMechanism
     @Binding var showingThmbnail: Bool
     @Binding var selectJapanese: Bool
-
+    
     @Environment(\.colorScheme) var colorScheme
-
+    
     var isDarkMode: Bool {
         return colorScheme == .dark
     }
-
+    
     var body: some View {
-        NavigationLink(
-            destination: ReactionDetailView(selectJapanese: selectJapanese, reactionMechanism: reactionMechanism)) {
-            VStack(alignment: .leading) {
-                Text(selectJapanese ? reactionMechanism.japanese : reactionMechanism.english)
-                    .foregroundColor(Color(.appMainText))
-                if showingThmbnail {
-                    ReactionListRowImage(imageUrl: reactionMechanism.thmbnailUrl, placeHolderName: "placeholder-list")
-                }
-                Divider()
+        VStack(alignment: .leading) {
+            Text(selectJapanese ? reactionMechanism.japanese : reactionMechanism.english)
+                .foregroundColor(Color(.appMainText))
+            if showingThmbnail {
+                ReactionListRowImage(imageUrl: reactionMechanism.thmbnailUrl, placeHolderName: "placeholder-list")
             }
         }
-        .padding(.top, 8)
-        .padding(.horizontal, 16)
     }
 }
 
--- Reaction/View/ReactionList/ReactionListView.swift
+++ Reaction/View/ReactionList/ReactionListView.swift
@@ -2,33 +2,32 @@ import SwiftUI
 
 struct ReactionListView: View {
     @StateObject var viewState: ReactionListViewState
-
+    
     var body: some View {
         NavigationStack {
             ZStack {
-                ScrollView {
-                    LazyVStack {
-                        ZStack(alignment: .trailing) {
-                            TextField("Type your search", text: $viewState.searchText)
-                                .padding(8)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                            if !viewState.searchText.isEmpty {
-                                Button(action: {
-                                    viewState.clearSearchText()
-                                }) {
-                                    Image(systemName: "delete.left")
-                                        .foregroundColor(Color(UIColor.opaqueSeparator))
-                                }
-                                .padding(.trailing, 12)
+                List {
+                    ZStack(alignment: .trailing) {
+                        TextField("Type your search", text: $viewState.searchText)
+                        if !viewState.searchText.isEmpty {
+                            Button(action: {
+                                viewState.clearSearchText()
+                            }) {
+                                Image(systemName: "delete.left")
+                                    .foregroundColor(Color(UIColor.opaqueSeparator))
                             }
                         }
-                        ForEach(viewState.showingReactions) { (reactionMechanism: ReactionMechanism) in
-                            ReactionListRow(reactionMechanism: reactionMechanism, showingThmbnail: $viewState.showingThmbnail, selectJapanese: $viewState.selectJapanese)
-                        }
                     }
-                    .padding(.bottom, 62)
+                    
+                    ForEach(viewState.showingReactions) { (reactionMechanism: ReactionMechanism) in
+                        NavigationLink(
+                            destination: ReactionDetailView(selectJapanese: viewState.selectJapanese, reactionMechanism: reactionMechanism)) {
+                                ReactionListRow(reactionMechanism: reactionMechanism, showingThmbnail: $viewState.showingThmbnail, selectJapanese: $viewState.selectJapanese)
+                            }
+                    }
                 }
-
+                .listStyle(.plain)
+                
                 if viewState.isFetching {
                     ProgressView()
                         .scaleEffect(1.5, anchor: .center)

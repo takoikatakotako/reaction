3c11bdd8d1bea1324f0e726f574e9d8b7e101d0b
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2024-07-21 22:52:46 +0900

    フォーマットを実装

--- Reaction/View/Common/CommonText.swift
+++ Reaction/View/Common/CommonText.swift
@@ -3,15 +3,14 @@ import SwiftUI
 struct CommonText: View {
     let text: String
     let font: Font
-    
+
     init(text: String, font: Font) {
         self.text = text
         self.font = font
     }
-    
+
     var body: some View {
         Text(text)
             .foregroundColor(Color(.appMainText))
     }
 }
-

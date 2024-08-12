4b9fbcd7058bcf34a40e4752d6c0bf78a62a8bbe
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2024-08-12 23:48:42 +0900

    バージョンコードを更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.swiswiswift.chemist"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

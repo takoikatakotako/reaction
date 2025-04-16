8d0de80afb605431b6ce565bbbf793a1d55f6c73
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2025-04-16 23:22:53 +0900

    レイアウトを実装

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -16,47 +16,47 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-      <div className="sub-cover">
-        <header className="page-header wrapper">
-          <h1 className="align-center">
-            <a href="index.html">
-              <p className="logo">シン反応機構管理画面</p>
-            </a>
-          </h1>
-          <nav>
-            <ul className="main-nav">
-              <li>
-                <a href="reaction.html" className="main-nav-active">
-                  反応機構
-                </a>
-              </li>
-              <li>
-                <a href="review.html" className="main-nav-inactive">
-                  学習問題
-                </a>
-              </li>
-              <li>
-                <a href="merchandise.html" className="main-nav-inactive">
-                  略称一覧
-                </a>
-              </li>
-              <li>
-                <a href="report.html" className="main-nav-inactive">
-                  報告
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </div>
+        <div className="sub-cover">
+          <header className="page-header wrapper">
+            <h1 className="align-center">
+              <a href="index.html">
+                <p className="logo">シン反応機構管理画面</p>
+              </a>
+            </h1>
+            <nav>
+              <ul className="main-nav">
+                <li>
+                  <a href="reaction.html" className="main-nav-active">
+                    反応機構
+                  </a>
+                </li>
+                <li>
+                  <a href="review.html" className="main-nav-inactive">
+                    学習問題
+                  </a>
+                </li>
+                <li>
+                  <a href="merchandise.html" className="main-nav-inactive">
+                    略称一覧
+                  </a>
+                </li>
+                <li>
+                  <a href="report.html" className="main-nav-inactive">
+                    報告
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+        </div>
 
         {children}
 
         <footer>
-        <div className="copyright">
-          <small>&copy; swiswiswift.com</small>
-        </div>
-      </footer>
+          <div className="copyright">
+            <small>&copy; swiswiswift.com</small>
+          </div>
+        </footer>
       </body>
     </html>
   );
--- admin/src/app/reaction/new/page.tsx
+++ admin/src/app/reaction/new/page.tsx
@@ -1,212 +1,361 @@
 'use client';
 
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, useRef, ChangeEvent, FormEvent } from 'react';
 
+interface MyFormData {
+  englishName: string;
+  japaneseName: string;
+  youtubeUrls: string[];
+}
 
 export default function AboutPage() {
+  const [formData, setFormData] = useState<MyFormData>({
+    englishName: '',
+    japaneseName: '',
+    youtubeUrls: [''],
+  });
 
-      const [name, setName] = useState<string>(''); // 入力された名前を文字列として管理
-    
-      // 入力変更時のイベントハンドラ
-      const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value);
+  const [englishName, setEnglishName] = useState<string>('');
+  const [japaneseName, setJapaneseName] = useState<string>('');
+  const [thumbnailImage, setThumbnailImage] = useState<string>('');
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // EnglishName
+  const englishNameHandleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEnglishName(e.target.value);
+  };
+
+  // JapaneseName
+  const japaneseNameHandleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setJapaneseName(e.target.value);
+  };
+
+  // Thumbnail
+  const thumbnailHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files && files.length > 0) {
+      const file = files[0];
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+        setThumbnailImage(base64String); // Base64をstateにセット
       };
-    
-      // フォーム送信時のイベントハンドラ
-      const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault(); // ページのリロードを防ぐ
-        alert(`送信された名前: ${name}`);
-        // ここでAPI通信や他の処理ができる
-      };
-    
-    
-    return (
-<main className="wrapper">
-<h1>反応機構追加</h1>
+      reader.readAsDataURL(file); // Base64に変換開始
+    }
 
-<form>
-    <div className="reaction-edit-content">
-        <label htmlFor="englishName">EnglishName</label>
-        {/* <input type="text" name="englishName" value="Acetoacetic Ester Synthesis" /> */}
-        <input
-          type="text"
-          name="englishName"
-          value={name}
-          onChange={handleChange}
-        />
-        <hr/>
-    </div>
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+  };
 
-    <div className="reaction-edit-content">
-        <label htmlFor="japanseeName">JapaneseName</label>
-        <input type="text" name="japanseeName" value="アセト酢酸エステル合成" />
-        <hr/>
-    </div>
+  // フォーム送信時のイベントハンドラ
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // ページのリロードを防ぐ
+    alert(`送信された名前: ${name}`);
+    // ここでAPI通信や他の処理ができる
+  };
 
-    <div className="reaction-edit-content">
-        <label htmlFor="thumbnail">Thumbnail</label>
+  return (
+    <main className="wrapper">
+      <h1>反応機構追加</h1>
 
-        <div className="reaction-edit-image-container" >
-            <img className="reaction-edit-image" src="/acetoacetic-ester-synthesis-thumbnail.png"/> 
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg"/></button>
+      <form>
+        <div className="reaction-edit-content">
+          <label htmlFor="englishName">EnglishName</label>
+          <input
+            type="text"
+            name="englishName"
+            placeholder="反応機構の英語名を入力"
+            value={englishName}
+            onChange={englishNameHandleChange}
+          />
+          <hr />
         </div>
-        <hr/>
-    </div>
 
-    <div className="reaction-edit-content">
-        <label htmlFor="thumbnail">General Formulas</label>
+        <div className="reaction-edit-content">
+          <label htmlFor="japanseeName">JapaneseName</label>
+          <input
+            type="text"
+            name="japanseeName"
+            placeholder="反応機構の日本語名を入力"
+            value={japaneseName}
+            onChange={japaneseNameHandleChange}
+          />
+          <hr />
+        </div>
 
-        <div className="reaction-edit-image-container" >
-            <img className="reaction-edit-image" src="/image-placeholder.png"/> 
+        <div className="reaction-edit-content">
+          <label htmlFor="thumbnail">Thumbnail</label>
+          <input
+            type="file"
+            multiple // 画像を複数選択できるようにする
+            accept="image/jpeg, image/png"
+            onChange={thumbnailHandleChange}
+            ref={inputRef}
+          />
+
+          {thumbnailImage === '' ? (
+            <div className="reaction-edit-image-container">
+              <img
+                className="reaction-edit-image"
+                src="/image-placeholder.png"
+              />
+            </div>
+          ) : (
+            <div className="reaction-edit-image-container">
+              <img className="reaction-edit-image" src={thumbnailImage} />
+              <button className="reaction-edit-image-delete-button">
+                <img src="/image-delete.svg" />
+              </button>
+            </div>
+          )}
+
+          <hr />
+        </div>
+
+        <div className="reaction-edit-content">
+          <label htmlFor="thumbnail">General Formulas</label>
+
+          <div className="reaction-edit-image-container">
+            <img className="reaction-edit-image" src="/image-placeholder.png" />
             {/*  <button class="reaction-edit-image-delete-button"><img src="/image-delete.svg"/></button> */}
+          </div>
+          <hr />
         </div>
-        <hr/>
-    </div>
 
-    <div className="reaction-edit-content">
-        <label htmlFor="thumbnail">Mechanisms</label>
+        <div className="reaction-edit-content">
+          <label htmlFor="thumbnail">Mechanisms</label>
 
-        <div className="reaction-edit-image-container" >
-            <img className="reaction-edit-image" src="/image-placeholder.png" /> 
+          <div className="reaction-edit-image-container">
+            <img className="reaction-edit-image" src="/image-placeholder.png" />
             {/* <button class="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button> */}
-        </div>
-        <hr/>
-    </div>
-
-    <div className="reaction-edit-content">
-        <label htmlFor="thumbnail">Examples</label>
-
-        <div className="reaction-edit-image-container" >
-            <img className="reaction-edit-image" src="/acetoacetic-ester-synthesis-thumbnail.png" /> 
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg"/></button>
+          </div>
+          <hr />
         </div>
 
-        <div className="reaction-edit-image-container" >
-            <img className="reaction-edit-image" src="/image-placeholder.png"/> 
+        <div className="reaction-edit-content">
+          <label htmlFor="thumbnail">Examples</label>
+
+          <div className="reaction-edit-image-container">
+            <img
+              className="reaction-edit-image"
+              src="/acetoacetic-ester-synthesis-thumbnail.png"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+
+          <div className="reaction-edit-image-container">
+            <img className="reaction-edit-image" src="/image-placeholder.png" />
             {/* <button class="reaction-edit-image-delete-button"><img src="/image-delete.svg"/></button>*/}
-        </div>
-        <hr/>
-    </div>
-
-    <div className="reaction-edit-content">
-        <label htmlFor="thumbnail">Supplements</label>
-
-        <div className="reaction-edit-image-container" >
-            <img className="reaction-edit-image" src="/acetoacetic-ester-synthesis-thumbnail.png" /> 
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button>
+          </div>
+          <hr />
         </div>
 
-        <div className="reaction-edit-image-container" >
-            <img className="reaction-edit-image" src="/image-placeholder.png" /> 
+        <div className="reaction-edit-content">
+          <label htmlFor="thumbnail">Supplements</label>
+
+          <div className="reaction-edit-image-container">
+            <img
+              className="reaction-edit-image"
+              src="/acetoacetic-ester-synthesis-thumbnail.png"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+
+          <div className="reaction-edit-image-container">
+            <img className="reaction-edit-image" src="/image-placeholder.png" />
             {/*  <button class="reaction-edit-image-delete-button"><img src="image/image-delete.svg"></button> */}
+          </div>
+          <hr />
         </div>
-        <hr/>
-    </div>
 
+        <div className="reaction-edit-content">
+          <label htmlFor="englishName">Suggestions</label>
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value="Acetoacetic Ester Synthesis"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
 
-    <div className="reaction-edit-content">
-        <label htmlFor="englishName">Suggestions</label>
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="Acetoacetic Ester Synthesis" />
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button>
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value="アセト酢酸エステル合成"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
+
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value=""
+              placeholder="サジェスチョンの単語を入力"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
+
+          <button className="reaction-edit-multi-input-plus-button">
+            <img src="/plus.svg" />
+          </button>
         </div>
-        <hr/>
 
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="アセト酢酸エステル合成" />
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button>
+        <div className="reaction-edit-content">
+          <label htmlFor="englishName">Reactants</label>
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value="Acetoacetic Ester Synthesis"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
+
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value="アセト酢酸エステル合成"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
+
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value=""
+              placeholder="サジェスチョンの単語を入力"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
+
+          <button className="reaction-edit-multi-input-plus-button">
+            <img src="/plus.svg" />
+          </button>
         </div>
-        <hr/>
 
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="" placeholder="サジェスチョンの単語を入力" />
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button>
+        <div className="reaction-edit-content">
+          <label htmlFor="englishName">Products</label>
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value="Acetoacetic Ester Synthesis"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
+
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value="アセト酢酸エステル合成"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
+
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value=""
+              placeholder="サジェスチョンの単語を入力"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
+
+          <button className="reaction-edit-multi-input-plus-button">
+            <img src="/plus.svg" />
+          </button>
         </div>
-        <hr/>
 
-        <button className="reaction-edit-multi-input-plus-button"><img src="/plus.svg" /></button>
-    </div>
+        <div className="reaction-edit-content">
+          <label htmlFor="englishName">Youtube</label>
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value="Acetoacetic Ester Synthesis"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="image/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
 
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value="アセト酢酸エステル合成"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="image/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
 
+          <div className="reaction-edit-multi-input-container">
+            <input
+              type="text"
+              name="englishName"
+              value=""
+              placeholder="サジェスチョンの単語を入力"
+            />
+            <button className="reaction-edit-image-delete-button">
+              <img src="image/image-delete.svg" />
+            </button>
+          </div>
+          <hr />
 
-    <div className="reaction-edit-content">
-        <label htmlFor="englishName">Reactants</label>
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="Acetoacetic Ester Synthesis" />
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg"/></button>
+          <button className="reaction-edit-multi-input-plus-button">
+            <img src="/plus.svg" />
+          </button>
+
+          <button className="reaction-edit-add-reaction-button">
+            <img src="/add-reaction.svg" />
+          </button>
         </div>
-        <hr/>
-
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="アセト酢酸エステル合成" />
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button>
-        </div>
-        <hr/>
-
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="" placeholder="サジェスチョンの単語を入力" />
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button>
-        </div>
-        <hr/>
-
-        <button className="reaction-edit-multi-input-plus-button"><img src="/plus.svg" /></button>
-    </div>
-
-
-    <div className="reaction-edit-content">
-        <label htmlFor="englishName">Products</label>
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="Acetoacetic Ester Synthesis" />
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button>
-        </div>
-        <hr/>
-
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="アセト酢酸エステル合成" />
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button>
-        </div>
-        <hr/>
-
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="" placeholder="サジェスチョンの単語を入力" />
-            <button className="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button>
-        </div>
-        <hr/>
-
-        <button className="reaction-edit-multi-input-plus-button"><img src="/plus.svg" /></button>
-    </div>
-
-
-
-    <div className="reaction-edit-content">
-        <label htmlFor="englishName">Youtube</label>
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="Acetoacetic Ester Synthesis" />
-            <button className="reaction-edit-image-delete-button"><img src="image/image-delete.svg" /></button>
-        </div>
-        <hr/>
-
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="アセト酢酸エステル合成" />
-            <button className="reaction-edit-image-delete-button"><img src="image/image-delete.svg" /></button>
-        </div>
-        <hr/>
-
-        <div className="reaction-edit-multi-input-container">
-            <input type="text" name="englishName" value="" placeholder="サジェスチョンの単語を入力" />
-            <button className="reaction-edit-image-delete-button"><img src="image/image-delete.svg" /></button>
-        </div>
-        <hr/>
-
-        <button className="reaction-edit-multi-input-plus-button"><img src="/plus.svg" /></button>
-
-        <button className="reaction-edit-add-reaction-button"><img src="/add-reaction.svg" /></button>
-
-    </div>
-
-</form> 
-
-</main>
-    );
-  }
-  
+      </form>
+    </main>
+  );
+}

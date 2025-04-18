783d99cf2a66145cc0f62b0354a3379d1995645d
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2025-04-18 23:56:36 +0900

    入力フォームを実装

--- admin/src/app/reaction/new/page.tsx
+++ admin/src/app/reaction/new/page.tsx
@@ -9,6 +9,9 @@ export default function AboutPage() {
   const [generalFormulaImages, setGeneralFormulaImages] = useState<string[]>(
     []
   );
+  const [mechanismasImages, setMechanismasImages] = useState<string[]>([]);
+  const [exampleImages, setExampleImages] = useState<string[]>([]);
+  const [supplementsImages, setSupplementsImages] = useState<string[]>([]);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -70,8 +73,84 @@ export default function AboutPage() {
   };
 
   // General Formulas Delete
-  const generalFormulasDeleteHandleChange =  (index: number) => {
+  const generalFormulasDeleteHandleChange = (index: number) => {
     setGeneralFormulaImages((prev) => prev.filter((_, idx) => idx !== index));
+  };
+
+  // Mechanisms
+  const mechanismsHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files && files.length > 0) {
+      const file = files[0];
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+        setMechanismasImages([...generalFormulaImages, base64String]);
+      };
+      reader.readAsDataURL(file);
+    }
+
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+  };
+
+  // Mechanisms Delete
+  const mechanismsDeleteHandleChange = (index: number) => {
+    setMechanismasImages((prev) => prev.filter((_, idx) => idx !== index));
+  };
+
+  // Examples
+  const examplesHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files && files.length > 0) {
+      const file = files[0];
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+        setExampleImages([...generalFormulaImages, base64String]);
+      };
+      reader.readAsDataURL(file);
+    }
+
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+  };
+
+  // Examples Delete
+  const examplessDeleteHandleChange = (index: number) => {
+    setExampleImages((prev) => prev.filter((_, idx) => idx !== index));
+  };
+
+
+  // Supplements
+  const supplementsHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files && files.length > 0) {
+      const file = files[0];
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+        setSupplementsImages([...generalFormulaImages, base64String]);
+      };
+      reader.readAsDataURL(file);
+    }
+
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+  };
+
+  // Supplements Delete
+  const supplementsDeleteHandleChange = (index: number) => {
+    setSupplementsImages((prev) => prev.filter((_, idx) => idx !== index));
   };
 
 
@@ -181,56 +260,113 @@ export default function AboutPage() {
           <hr />
         </div>
 
+        {/* Mechanisms */}
         <div className="reaction-edit-content">
           <label htmlFor="thumbnail">Mechanisms</label>
+          <input
+            type="file"
+            accept="image/png"
+            onChange={mechanismsHandleChange}
+            ref={inputRef}
+          />
 
-          <div className="reaction-edit-image-container">
-            <img className="reaction-edit-image" src="/image-placeholder.png" />
-            {/* <button class="reaction-edit-image-delete-button"><img src="/image-delete.svg" /></button> */}
-          </div>
+          {mechanismasImages.length === 0 && (
+            <div className="reaction-edit-image-container">
+              <img
+                className="reaction-edit-image"
+                src="/image-placeholder.png"
+              />
+            </div>
+          )}
+
+          {mechanismasImages.length !== 0 &&
+            mechanismasImages.map((image, idx) => (
+              <div className="reaction-edit-image-container">
+                <img className="reaction-edit-image" src={image} />
+                <button
+                  type="button"
+                  className="reaction-edit-image-delete-button"
+                  onClick={() => mechanismsDeleteHandleChange(idx)}
+                >
+                  <img src="/image-delete.svg" />
+                </button>
+              </div>
+            ))}
           <hr />
         </div>
 
+        {/* Examples */}
         <div className="reaction-edit-content">
           <label htmlFor="thumbnail">Examples</label>
+          <input
+            type="file"
+            accept="image/png"
+            onChange={examplesHandleChange}
+            ref={inputRef}
+          />
 
-          <div className="reaction-edit-image-container">
-            <img
-              className="reaction-edit-image"
-              src="/acetoacetic-ester-synthesis-thumbnail.png"
-            />
-            <button className="reaction-edit-image-delete-button">
-              <img src="/image-delete.svg" />
-            </button>
-          </div>
+          {exampleImages.length === 0 && (
+            <div className="reaction-edit-image-container">
+              <img
+                className="reaction-edit-image"
+                src="/image-placeholder.png"
+              />
+            </div>
+          )}
 
-          <div className="reaction-edit-image-container">
-            <img className="reaction-edit-image" src="/image-placeholder.png" />
-            {/* <button class="reaction-edit-image-delete-button"><img src="/image-delete.svg"/></button>*/}
-          </div>
+          {exampleImages.length !== 0 &&
+            exampleImages.map((image, idx) => (
+              <div className="reaction-edit-image-container">
+                <img className="reaction-edit-image" src={image} />
+                <button
+                  type="button"
+                  className="reaction-edit-image-delete-button"
+                  onClick={() => examplessDeleteHandleChange(idx)}
+                >
+                  <img src="/image-delete.svg" />
+                </button>
+              </div>
+            ))}
+
           <hr />
         </div>
 
+        {/* Supplements */}
         <div className="reaction-edit-content">
           <label htmlFor="thumbnail">Supplements</label>
+          <input
+            type="file"
+            accept="image/png"
+            onChange={supplementsHandleChange}
+            ref={inputRef}
+          />
 
-          <div className="reaction-edit-image-container">
-            <img
-              className="reaction-edit-image"
-              src="/acetoacetic-ester-synthesis-thumbnail.png"
-            />
-            <button className="reaction-edit-image-delete-button">
-              <img src="/image-delete.svg" />
-            </button>
-          </div>
+          {supplementsImages.length === 0 && (
+            <div className="reaction-edit-image-container">
+              <img
+                className="reaction-edit-image"
+                src="/image-placeholder.png"
+              />
+            </div>
+          )}
 
-          <div className="reaction-edit-image-container">
-            <img className="reaction-edit-image" src="/image-placeholder.png" />
-            {/*  <button class="reaction-edit-image-delete-button"><img src="image/image-delete.svg"></button> */}
-          </div>
+          {supplementsImages.length !== 0 &&
+            supplementsImages.map((image, idx) => (
+              <div className="reaction-edit-image-container">
+                <img className="reaction-edit-image" src={image} />
+                <button
+                  type="button"
+                  className="reaction-edit-image-delete-button"
+                  onClick={() => supplementsDeleteHandleChange(idx)}
+                >
+                  <img src="/image-delete.svg" />
+                </button>
+              </div>
+            ))}
           <hr />
         </div>
 
+        {/* Suggestions */}
         <div className="reaction-edit-content">
           <label htmlFor="englishName">Suggestions</label>
           <div className="reaction-edit-multi-input-container">

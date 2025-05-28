076fcdb8a8dfe5d2a208d203e6df81847d49c979
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2025-05-28 23:39:35 +0900

    roleのIAMを訂正

--- terraform/modules/github_for_app/main.tf
+++ terraform/modules/github_for_app/main.tf
@@ -1,6 +1,6 @@
 # App CI Role
 resource "aws_iam_role" "app_ci_role" {
-  name               = "management-ci-role"
+  name               = "reaction-ci-role"
   assume_role_policy = data.aws_iam_policy_document.app_ci_role_assume_policy_document.json
 }
 
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "app_ci_role_assume_policy_document" {
 }
 
 resource "aws_iam_policy" "app_ci_role_policy" {
-  name   = "app-ci-role-policy"
+  name   = "reaction-ci-role-policy"
   policy = data.aws_iam_policy_document.app_ci_role_policy_document.json
 }
 
@@ -37,4 +37,9 @@ data "aws_iam_policy_document" "app_ci_role_policy_document" {
 resource "aws_iam_role_policy_attachment" "app_ci_role_policy_attachment" {
   role       = aws_iam_role.app_ci_role.name
   policy_arn = aws_iam_policy.app_ci_role_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access_attachment" {
+  role       = aws_iam_role.app_ci_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }

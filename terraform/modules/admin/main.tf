3d1cf5ab1c9f90898512bd9a68effc1dbb732c96
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2025-06-19 18:24:17 +0900

    Terraformのフォーマットを行った。

--- terraform/modules/admin/main.tf
+++ terraform/modules/admin/main.tf
@@ -82,8 +82,8 @@ resource "aws_cloudfront_function" "basic_auth_function" {
   name    = "admin-basic-auth-function"
   publish = true
   runtime = "cloudfront-js-2.0"
-  code    = templatefile("${path.module}/basic-auth-function.js", {
-    admin_user = var.admin_user,
+  code = templatefile("${path.module}/basic-auth-function.js", {
+    admin_user     = var.admin_user,
     admin_password = var.admin_password,
   })
 }
